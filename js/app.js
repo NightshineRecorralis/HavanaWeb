0b8b48a92b0c89deed5378a3a3fc8e6ad8f3805f
Pixelific <pixelificz@gmail.com>
2019-10-17 23:15:09 -0400

    remove unneeded js

--- js/app.js
+++ js/app.js
@@ -21,48 +21,7 @@
 	});
 
 })(jQuery, document, window);
-
-// Modal code
-
-var modalNew = document.getElementById('imgModalDiv');
-var imagesNew = document.getElementsByClassName('imagelist-img');
-var captionTextNew = document.getElementById("captionModal");
-
-
-for (var i = 0; i < imagesNew.length; i++) {
-	var img = imagesNew[i];
-	img.onclick = function(evt) {
-		modalNew.style.display = "block";
-		captionTextNew.innerHTML = this.alt;
-	}
-}
-
-// Switching images in Modal code
-
-var slideIndex = 1;
-var currentSlide = 1;
-var currentSlideInt = 0;
-
-function showCurrentImage(clicked_id) {
-	currentSlide = clicked_id;
-	slideIndex = currentSlide; 
-	currentSlideInt = parseInt(currentSlide);
-}
-              
-showDivs(currentSlideInt);
-              
-function plusDivs(y) { showDivs(currentSlideInt += y); }
-function currentDiv(n) { showDivs(currentSlideInt); }
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("modalSlides");
-	if (n > x.length) {currentSlideInt = 1;}    
-    if (n < 1) {currentSlideInt = x.length}
-    for (i = 0; i < x.length; i++) { x[i].style.display = "none"; }	
-	x[currentSlideInt-1].style.display = "block";  
-}
-
+  
 function showMapInfo() {
 	var x = document.getElementById("mapInfo");
 	if (x.style.display === "none") {
